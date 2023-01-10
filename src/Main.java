7b7b707a4d83e1b0d8c17844de2b22d7e3d7af91
Anastasia <asiaa2110@gmail.com>
2023-01-10 19:21:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int initialBalance = 200; // Начальный счет баланса
+        int replenishmentAmount = 1300; // Пополнение счета
+        int threshold = 1000; // Порог пополнения для начисления бонусов
+        int bonus = 0;
+        if (replenishmentAmount > threshold) {
+            bonus = replenishmentAmount / 100;
+        }
+
+
+            System.out.println((replenishmentAmount + initialBalance + bonus)+ " итоговый счёт");
+            System.out.println((bonus)+ " бонусные рубли");
+        }
+    }
