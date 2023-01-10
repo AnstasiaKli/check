5d6bb8d40171b812eb6059e3e4d6afed96f2bf48
Anastasia <asiaa2110@gmail.com>
2023-01-10 19:30:58 +0300

    Main.java changed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int initialBalance = 200; // Начальный счет баланса
-        int replenishmentAmount = 1300; // Пополнение счета
+        int replenishmentAmount = 400; // Пополнение счета
         int threshold = 1000; // Порог пополнения для начисления бонусов
         int bonus = 0;
         if (replenishmentAmount > threshold) {
